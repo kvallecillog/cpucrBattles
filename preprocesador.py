43691141ecf46e2cf848214cbf185d43ea5f4d0e
kenneth <kvallecillog@gmail.com>
2015-08-29 19:39:04 -0600

    Preprocessor class 1st .py file

---
+++ preprocesador.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+"""Preprocessor .ASM files for CPUCR Architecture.
+"""
+
+__author__ = "Kenneth Vallecillo Gonzalez"
+__copyright__ = "Copyright 2015"
+__credits__ = ["Lochi Yu", "Enrique Coen", "Diego Valverde"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Kenneth Vallecillo Gonzalez"
+__email__ = "kvallecillog@gmail.com"
+__status__ = "WIP: Working In Progress"
+
+
+# Se importa la biblioteca para las expresiones regulares.
+import re
+
+
+def comments_proccessor (
+
+raw_line,
+proccess_line
+
+	):
+
+	# Do something
+
+	return 0
