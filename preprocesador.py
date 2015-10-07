832a63bb2ffdf93684e8485aa1af34f22084ac5c
kenneth <kvallecillog@gmail.com>
2015-10-06 22:35:51 -0600

    Init data recollector and checker

--- preprocesador.py
+++ preprocesador.py
@@ -122,16 +122,62 @@ def delete_spaces(data_list):
     return data_list
 
 
-def label_checker(data_list, lines_raw_list):
-    print("Checking labels\n")
+def init_checker(data_list, lines_raw_list):
 
-    # Se identifica si se esta utilizando como etiqueta una palabra reservada.
+
+    regex_pos_def = re.compile(r"\b^(\*)\b(\=)\b((\@)[0-7]{1,4})\b$", re.IGNORECASE)
+    regex_pos_assign = re.compile(r"\b^([a-zA-Z](\w{1,7})?)\b(\=)\b(\*)\b$", re.IGNORECASE)
+
+    pos_def_match = re.match(regex_pos_def, non_num_line)
+    pos_assign_match = re.match(regex_pos_assign, non_num_line)
+
 
     regex_res_word = re.compile(r"\b(AND|BCC|BCS|BEQ|BMI|BNE|BPL|BVC|BVS|CLA|CLC|CLI|CPA|\
                      DCA|HLT|INA|INP|JMP|JSR|LDA|NOP|ORA|OUT|PHA|PHS|PLA|PLS|ROL|ROR|RTI|\
                      RTS|SEC|SEI|STA|SUB|TAP|TPA)\b", re.IGNORECASE)
 
-    regex_label_abs_inst = re.compile(r'^([a-zA-Z](\w{1,7})?)\s(LDA|STA|ADD|SUB|AND|ORA|JMP|JSR)\b(\s)((\@)[0-7]{1,4}|(([a-zA-Z](\w{1,7})?)(\+?)(\d*)))$', re.IGNORECASE)
+    for x in range(0, len(data_list)):
+
+        data_list_x = ''.join(data_list[x])
+        num_line = data_list_x.split(" ")
+        non_num_line = " ".join(num_line[1:len(num_line)])
+        num_line_int = int(num_line[0])
+        num_line_int_0 = int(num_line[0]) - 1
+        data_source_line = lines_raw_list[num_line_int_0]
+        data_source_line_list = data_source_line.split(" ")
+        data_source_line_n = " ".join(data_source_line_list[1:len(data_source_line_list)])
+
+        cont_res_word_dic = Counter(w.lower() for w in re.findall(regex_res_word, non_num_line))
+        print(cont_res_word_dic)
+        cont_res_word_int = sum(cont_res_word_dic.values())
+
+        if cont_res_word_int == 0:
+            print("ok")
+
+        else:
+            # raise Exception('Error: Reserved word as label')
+            print("Error!:"+str(cont_res_word_int-1)+" Reserved words as a label")
+            print(num_line_int,"|",non_num_line)
+
+    hash_init = []
+
+    return hash_init, data_list
+
+
+def label_checker(data_list, lines_raw_list, hash_init):
+    print("Checking labels\n")
+
+    # Se identifica si se esta utilizando como etiqueta una palabra reservada.
+
+    regex_macro = re.compile(r"\b^(MACRO|FINMAC)\b(\s)*.*", re.IGNORECASE)
+
+
+
+    regex_res_word = re.compile(r"\b(AND|BCC|BCS|BEQ|BMI|BNE|BPL|BVC|BVS|CLA|CLC|CLI|CPA|\
+                     DCA|HLT|INA|INP|JMP|JSR|LDA|NOP|ORA|OUT|PHA|PHS|PLA|PLS|ROL|ROR|RTI|\
+                     RTS|SEC|SEI|STA|SUB|TAP|TPA)\b", re.IGNORECASE)
+
+    regex_label_abs_inst = re.compile(r'\b^([a-zA-Z](\w{1,7})?)\b(\s)\b(LDA|STA|ADD|SUB|AND|ORA|JMP|JSR)\b(\s)\b((\@)[0-7]{1,4}|(([a-zA-Z](\w{1,7})?)(\+?)(\d*)))\b$', re.IGNORECASE)
     regex_inst_abs = re.compile(r'\b^(LDA|STA|ADD|SUB|AND|ORA|JMP|JSR)\b(\s)((\@)[0-7]{1,4}|([a-zA-Z](\w{1,7})?)(\+?)(\d*))$', re.IGNORECASE)
 
     regex_label_ind_inst = re.compile(r'^([a-zA-Z](\w{1,7})?)\s(LDA|STA|ADD|SUB|AND|ORA|JMP|JSR)\b(\s)(\()((\@)[0-7]{1,4}|(([a-zA-Z](\w{1,7})?)(\+?)(\d*)))(\))$', re.IGNORECASE)
@@ -155,7 +201,6 @@ def label_checker(data_list, lines_raw_list):
     regex_label_imp_inst = re.compile(r'^([a-zA-Z](\w{1,7})?)\s(SEC|CLC|SEI|CLI)$', re.IGNORECASE)
     regex_inst_imp = re.compile(r'\b^(SEC|CLC|SEI|CLI)\b$', re.IGNORECASE)
 
-
     for x in range(0, len(data_list)):
 
         data_list_x = ''.join(data_list[x])
@@ -167,134 +212,147 @@ def label_checker(data_list, lines_raw_list):
         data_source_line_list = data_source_line.split(" ")
         data_source_line_n = " ".join(data_source_line_list[1:len(data_source_line_list)])
 
-        cont_res_word_dic = Counter(w.lower() for w in re.findall(regex_res_word, non_num_line))
-        print(cont_res_word_dic)
-        cont_res_word_int = sum(cont_res_word_dic.values())
 
-        if cont_res_word_int > 1:
 
-            # raise Exception('Error: Reserved word as label')
-            print("Error!:"+str(cont_res_word_int-1)+" Reserved words as a label")
-            print(num_line_int,"|",non_num_line)
+        macro_match = re.match(regex_macro, non_num_line)
 
-        elif cont_res_word_int == 1:
-            print("OK!: Labels and instrucions are valid")
 
-            label_inst_abs_match = re.match(regex_label_abs_inst, non_num_line)
-            inst_abs_match = re.match(regex_inst_abs, non_num_line)
+        if not macro_match:
 
-            label_inst_ind_match = re.match(regex_label_ind_inst, non_num_line)
-            inst_ind_match = re.match(regex_inst_ind, non_num_line)
+            cont_res_word_dic = Counter(w.lower() for w in re.findall(regex_res_word, non_num_line))
+            print(cont_res_word_dic)
+            cont_res_word_int = sum(cont_res_word_dic.values())
 
-            label_inst_inm_match = re.match(regex_label_inm_inst, non_num_line)
-            inst_inm_match = re.match(regex_inst_inm, non_num_line)
+            if cont_res_word_int > 1:
 
-            label_inst_io_match = re.match(regex_label_io_inst, non_num_line)
-            inst_io_match = re.match(regex_inst_io, non_num_line)
+                # raise Exception('Error: Reserved word as label')
+                print("Error!:"+str(cont_res_word_int-1)+" Reserved words as a label")
+                print(num_line_int,"|",non_num_line)
 
-            label_inst_rel_match = re.match(regex_label_rel_inst, non_num_line)
-            inst_rel_match = re.match(regex_inst_rel, non_num_line)
+            elif cont_res_word_int == 1:
+                print("OK!: Labels and instrucions are valid")
 
-            label_inst_acum_match = re.match(regex_label_acum_inst, non_num_line)
-            inst_acum_match = re.match(regex_inst_acum, non_num_line)
+                label_inst_abs_match = re.match(regex_label_abs_inst, non_num_line)
+                inst_abs_match = re.match(regex_inst_abs, non_num_line)
 
-            label_inst_ctrl_match = re.match(regex_label_ctrl_inst, non_num_line)
-            inst_ctrl_match = re.match(regex_inst_ctrl, non_num_line)
+                label_inst_ind_match = re.match(regex_label_ind_inst, non_num_line)
+                inst_ind_match = re.match(regex_inst_ind, non_num_line)
 
-            label_inst_imp_match = re.match(regex_label_imp_inst, non_num_line)
-            inst_imp_match = re.match(regex_inst_imp, non_num_line)
+                label_inst_inm_match = re.match(regex_label_inm_inst, non_num_line)
+                inst_inm_match = re.match(regex_inst_inm, non_num_line)
 
-            if inst_abs_match or inst_ind_match or inst_inm_match or inst_io_match \
-                    or inst_rel_match or inst_acum_match or inst_ctrl_match or inst_imp_match:
+                label_inst_io_match = re.match(regex_label_io_inst, non_num_line)
+                inst_io_match = re.match(regex_inst_io, non_num_line)
 
-                print("Cumple con la condicion de instruccion")
-                if inst_abs_match:
+                label_inst_rel_match = re.match(regex_label_rel_inst, non_num_line)
+                inst_rel_match = re.match(regex_inst_rel, non_num_line)
 
-                    print("OPERANDO!:",inst_abs_match.group(3))
-                    print("Es una instruccion direccionamiento pura absoluto")
-                    print(num_line_int,"|", data_source_line_n)
+                label_inst_acum_match = re.match(regex_label_acum_inst, non_num_line)
+                inst_acum_match = re.match(regex_inst_acum, non_num_line)
 
-                if inst_ind_match:
+                label_inst_ctrl_match = re.match(regex_label_ctrl_inst, non_num_line)
+                inst_ctrl_match = re.match(regex_inst_ctrl, non_num_line)
 
-                    # print("OPERANDO!:",inst_abs_match.group(3))
-                    print("Es una instruccion direccionamiento pura indirecto")
-                    print(num_line_int,"|", data_source_line_n)
-                if inst_inm_match:
+                label_inst_imp_match = re.match(regex_label_imp_inst, non_num_line)
+                inst_imp_match = re.match(regex_inst_imp, non_num_line)
 
-                    # print("OPERANDO!:",inst_abs_match.group(3))
-                    print("Es una instruccion direccionamiento pura inmediato")
-                    print(num_line_int,"|", data_source_line_n)
+                if inst_abs_match or inst_ind_match or inst_inm_match or inst_io_match \
+                        or inst_rel_match or inst_acum_match or inst_ctrl_match or inst_imp_match:
 
-                if inst_io_match:
+                    print("Cumple con la condicion de instruccion")
+                    if inst_abs_match:
 
-                    # print("OPERANDO!:",inst_abs_match.group(3))
-                    print("Es una instruccion direccionamiento pura entrada/salida")
-                    print(num_line_int,"|", data_source_line_n)
-
-                if inst_rel_match:
-
-                    # print("OPERANDO!:",inst_abs_match.group(3))
-                    print("Es una instruccion direccionamiento pura relativo")
-                    print(num_line_int,"|", data_source_line_n)
-
-                if inst_acum_match:
-
-                    # print("OPERANDO!:",inst_abs_match.group(1))
-                    print("Es una instruccion direccionamiento pura acumulador")
-                    print(num_line_int,"|", data_source_line_n)
-
-                if inst_ctrl_match:
-
-                    # print("OPERANDO!:",inst_abs_match.group(2))
-                    print("Es una instruccion direccionamiento pura control")
-                    print(num_line_int,"|", data_source_line_n)
-
-                if inst_imp_match:
-
-                    # print("OPERANDO!:",inst_abs_match.group(1))
-                    print("Es una instruccion direccionamiento pura implicito")
-                    print(num_line_int,"|", data_source_line_n)
-
-            else:
-
-                if label_inst_abs_match or label_inst_ind_match or \
-                        label_inst_inm_match or label_inst_io_match \
-                        or label_inst_rel_match or label_inst_acum_match \
-                        or label_inst_ctrl_match or label_inst_imp_match:
-                    print("Cumple con la condicion de etiqueta de 8 caracteres + instruccion")
-
-                    if label_inst_abs_match:
-                        print("Es una instruccion de direccionamiento absoluto")
+                        print("OPERANDO!:",inst_abs_match.group(3))
+                        print("Es una instruccion direccionamiento pura absoluto")
                         print(num_line_int,"|", data_source_line_n)
-                    if label_inst_ind_match:
-                        print("Es una instruccion de direccionamiento indirecto")
+
+                    if inst_ind_match:
+
+                        # print("OPERANDO!:",inst_abs_match.group(3))
+                        print("Es una instruccion direccionamiento pura indirecto")
                         print(num_line_int,"|", data_source_line_n)
-                    if label_inst_inm_match:
-                        print("Es una instruccion de direccionamiento inmediato")
+                    if inst_inm_match:
+
+                        # print("OPERANDO!:",inst_abs_match.group(3))
+                        print("Es una instruccion direccionamiento pura inmediato")
                         print(num_line_int,"|", data_source_line_n)
-                    if label_inst_io_match:
-                        print("Es una instruccion de direccionamiento entrada/salida")
+
+                    if inst_io_match:
+
+                        # print("OPERANDO!:",inst_abs_match.group(3))
+                        print("Es una instruccion direccionamiento pura entrada/salida")
                         print(num_line_int,"|", data_source_line_n)
-                    if label_inst_rel_match:
-                        print("Es una instruccion de direccionamiento relativo")
+
+                    if inst_rel_match:
+
+                        # print("OPERANDO!:",inst_abs_match.group(3))
+                        print("Es una instruccion direccionamiento pura relativo")
                         print(num_line_int,"|", data_source_line_n)
-                    if label_inst_acum_match:
-                        print("Es una instruccion de direccionamiento acumulador")
+
+                    if inst_acum_match:
+
+                        # print("OPERANDO!:",inst_abs_match.group(1))
+                        print("Es una instruccion direccionamiento pura acumulador")
                         print(num_line_int,"|", data_source_line_n)
-                    if label_inst_ctrl_match:
-                        print("Es una instruccion de direccionamiento control")
+
+                    if inst_ctrl_match:
+
+                        # print("OPERANDO!:",inst_abs_match.group(2))
+                        print("Es una instruccion direccionamiento pura control")
                         print(num_line_int,"|", data_source_line_n)
-                    if label_inst_imp_match:
-                        print("Es una instruccion de direccionamiento implicito")
+
+                    if inst_imp_match:
+
+                        # print("OPERANDO!:",inst_abs_match.group(1))
+                        print("Es una instruccion direccionamiento pura implicito")
                         print(num_line_int,"|", data_source_line_n)
 
                 else:
-                    print("Error: No es un formato de etiqueta + instruccion valido.")
-                    print(num_line_int,"|", data_source_line_n)
 
+                    if label_inst_abs_match or label_inst_ind_match or \
+                            label_inst_inm_match or label_inst_io_match \
+                            or label_inst_rel_match or label_inst_acum_match \
+                            or label_inst_ctrl_match or label_inst_imp_match:
+                        print("Cumple con la condicion de etiqueta de 8 caracteres + instruccion")
+
+                        if label_inst_abs_match:
+
+                            print("OPERANDO!:",label_inst_abs_match.group(6))
+                            print("Es una instruccion de direccionamiento absoluto")
+                            print(num_line_int,"|", data_source_line_n)
+
+                        if label_inst_ind_match:
+                            print("Es una instruccion de direccionamiento indirecto")
+                            print(num_line_int,"|", data_source_line_n)
+                        if label_inst_inm_match:
+                            print("Es una instruccion de direccionamiento inmediato")
+                            print(num_line_int,"|", data_source_line_n)
+                        if label_inst_io_match:
+                            print("Es una instruccion de direccionamiento entrada/salida")
+                            print(num_line_int,"|", data_source_line_n)
+                        if label_inst_rel_match:
+                            print("Es una instruccion de direccionamiento relativo")
+                            print(num_line_int,"|", data_source_line_n)
+                        if label_inst_acum_match:
+                            print("Es una instruccion de direccionamiento acumulador")
+                            print(num_line_int,"|", data_source_line_n)
+                        if label_inst_ctrl_match:
+                            print("Es una instruccion de direccionamiento control")
+                            print(num_line_int,"|", data_source_line_n)
+                        if label_inst_imp_match:
+                            print("Es una instruccion de direccionamiento implicito")
+                            print(num_line_int,"|", data_source_line_n)
+
+                    else:
+                        print("Error: No es un formato de etiqueta + instruccion valido.")
+                        print(num_line_int,"|", data_source_line_n)
+
+            else:
+                print("Error!: no instruction in the line")
+                print(num_line_int,"|", data_source_line_n)
         else:
-            print("Error!: instruction in the line")
-            print(num_line_int,"|", data_source_line_n)
+            print("Error: Macro is not supported")
+            print(num_line_int,"|",non_num_line)
 
 
 def instruction_checker(data_list, lines_raw_list):
